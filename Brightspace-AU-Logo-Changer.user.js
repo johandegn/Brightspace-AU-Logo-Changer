49a69b45f00f71c3e3ed9da6aa37e0b53f911963
Johan Degn <54548365+johandegn@users.noreply.github.com>
2022-01-17 01:29:51 +0100

    Added default option to fade in and out.

--- Brightspace-AU-Logo-Changer.user.js
+++ Brightspace-AU-Logo-Changer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Brightspace AU Logo Changer
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  Make the Brightspace logo a little happier
 // @namespace    https://github.com/johandegn/Brightspace-AU-Logo-Changer
 // @match        https://brightspace.au.dk/*
@@ -15,16 +15,47 @@ const img_map = new Map();
 img_map.set('https://brightspace.au.dk/d2l/lp/navbars/6606/theme/viewimage/780/view', logo);
 img_map.set('https://brightspace.au.dk/d2l/lp/navbars/6606/theme/viewimage/779/view', home);
 
-function init() {
+const fade_speed = 25;
+const fade = true; // Change manually
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fadeIn(elm) {
+    let op = elm.style.opacity = 0;
+    while(op < 1) {
+        await sleep(fade_speed);
+        op += 0.15;
+        elm.style.opacity = op;
+    }
+    elm.style.opacity = 1;
+}
+
+async function fadeOut(elm) {
+    let op = elm.style.opacity = 1;
+    while(op > 0) {
+        await sleep(fade_speed);
+        op -= 0.15;
+        elm.style.opacity = op;
+    }
+    elm.style.opacity = 0;
+}
+
+async function init() {
     let img = document.getElementsByClassName("d2l-navigation-s-logo")[0].firstChild.shadowRoot.lastChild.firstChild.firstChild;
     if (img.src === undefined) {
         return;
     }
     let cur_src = img.src.split("?")[0];
     let new_src = img_map.get(cur_src);
-    if (new_src !== undefined) {
-        img.src = new_src;
+    if (new_src === undefined) {
+        return;
     }
+
+    if (fade) {await fadeOut(img);}
+    img.src = new_src;
+    if (fade) {await fadeIn(img);}
 }
 init();
 //window.onload = init;
