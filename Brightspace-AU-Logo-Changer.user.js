a8156b03a83fc5ba189934a3b353d9eb8b452cfa
Johan Degn <54548365+johandegn@users.noreply.github.com>
2022-01-18 01:00:47 +0100

    Fixed init for cross platform (i think)

--- Brightspace-AU-Logo-Changer.user.js
+++ Brightspace-AU-Logo-Changer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Brightspace AU Logo Changer
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Make the Brightspace logo a little happier
 // @namespace    https://github.com/johandegn/Brightspace-AU-Logo-Changer
 // @match        https://brightspace.au.dk/*
@@ -43,15 +43,18 @@ async function fadeOut(elm) {
 }
 
 async function init() {
-    let img = document.getElementsByClassName('d2l-navigation-s-logo')[0].firstChild.shadowRoot.lastChild.firstChild.firstChild;
-    if (img.src === undefined) {
-        return;
+    while (document.readyState !== 'complete') {
+        await sleep(100);
     }
+
+    let img = document.getElementsByClassName('d2l-navigation-s-logo')[0].firstChild.shadowRoot.lastChild.firstChild.firstChild;
     let cur_src = img.src.split("?")[0];
     let new_src = img_map.get(cur_src);
     if (new_src === undefined) {
+        console.log('Could not find matching image');
         return;
     }
+
     if (fade) {
         await fadeOut(img);
         img.addEventListener('load', async function () {
